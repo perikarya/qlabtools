7ff3c490ee4ead4a453928f060911e778e6ea380
perikarya <somayra.mamsa@gmail.com>
2020-08-09 19:32:24 +0800

    init

---
+++ R/calc_mass.R
@@ -0,0 +1,39 @@
+#' Calculate the mass of a substance containing a specified number of moles.
+#'
+#' @description Calculates the mass of a specified substance in grams that will contain the specified number of moles.
+#'
+#' @param substance Enter the common name of the chemical compound or substance.
+#' @examples
+#' "ammonium"
+#'
+#' @param val The number of moles.
+#'
+#' @param u The unit, select from "mol", "umol", "pmol", "fmol" with default set to moles.
+#'
+#' @return The mass of the substance containing the specified number of moles, in grams.
+#'
+#' @import webchem
+#'
+#' @export
+
+calc_mass <- function(substance, val, u="mol"){
+  requireNamespace()
+  if (u=="mmol"){
+    val <- val / 1000
+  }
+  else if (u=="umol"){
+    val <- val / 1000000
+  }
+  else if (u=="pmol"){
+    val <- val / 1000000000
+  }
+  else if (u=="fmol"){
+    val <- val / 1000000000000
+  }
+  else if (u != "" && u != "mol" && u != "mmol" && u != "umol" && u != "pmol" && u != "fmol"){
+    stop("Acceptable units are mol, mmol, umol, pmol and fmol.")
+  }
+  ccid <- get_cid(substance)[2]
+  mw <- pc_prop(as.numeric(ccid), properties="MolecularWeight")[2]
+  print(paste(as.numeric(val * mw), "grams", sep=" "))
+}
---
+++ R/calc_mol.R
@@ -0,0 +1,39 @@
+#' Calculate the number of moles in a specified amount of a chemical substance.
+#'
+#' @description Calculates the number of moles in a specified amount of a chemical substance.
+#'
+#' @param substance Enter the common name of the chemical compound or substance.
+#' @examples
+#' "ammonium"
+#'
+#' @param mass The mass.
+#'
+#' @param u The unit, select from "g", "ug", "pg", "fg" with default set to grams
+#'
+#' @return The number of moles of substance in the specified mass.
+#'
+#' @import webchem
+#'
+#' @export
+
+calc_mol <- function(substance, mass, u="g"){
+  requireNamespace()
+  if (u=="mg"){
+    mass <- mass / 1000
+  }
+  else if (u=="ug"){
+    mass <- mass / 1000000
+  }
+  else if (u=="pg"){
+    mass <- mass / 1000000000
+  }
+  else if (u=="fg"){
+    mass <- mass / 1000000000000
+  }
+  else if (u != "" && u != "g" && u != "mg" && u != "ug" && u != "pg" && u != "fg"){
+    stop("Acceptable units are g, mg, ug, pg and fg.")
+  }
+  ccid <- get_cid(substance)[2]
+  mw <- pc_prop(as.numeric(ccid), properties="MolecularWeight")[2]
+  print(paste("Number of moles:", as.numeric(mass / mw)), sep=" ")
+}
